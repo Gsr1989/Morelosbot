ba3db006f7caf1e8b15df935525e5af6fefe79be
Gsr1989 <empresalorens@gmail.com>
2025-12-07 23:58:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1021,4 +1021,4 @@ app.router.lifespan_context = lifespan
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000"))) 
